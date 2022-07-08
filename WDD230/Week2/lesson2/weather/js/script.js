9bd82713d37ffdad957b296c044312c600a9cced
MugDuck <107092969+MugDuck@users.noreply.github.com>
2022-07-08 03:34:21 -0700

    Add files via upload

--- WDD230/Week2/lesson2/weather/js/script.js
+++ WDD230/Week2/lesson2/weather/js/script.js
@@ -1,6 +1,6 @@
 var coll = document.getElementsByClassName("menu");
 var i;
-console.log("active")
+
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
